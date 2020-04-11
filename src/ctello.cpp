20418c7c6adace8fc6f6345eb5a617219e617a59
Carlos Perez-Lopez <carlospzlz@gmail.com>
2020-04-11 13:53:49 +0100

    Added GPLv3.0 license

--- src/ctello.cpp
+++ src/ctello.cpp
@@ -1,3 +1,21 @@
+//  CTello is a C++ library to interact with the DJI Ryze Tello Drone
+//  Copyright (C) 2020 Carlos Perez-Lopez
+//
+//  This library is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>
+//
+//  You can contact the author via carlospzlz@gmail.com
+
 #include "ctello.h"
 
 #include <arpa/inet.h>
@@ -12,8 +30,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include <sstream>
 #include <iostream>
+#include <sstream>
 
 namespace
 {
@@ -72,9 +90,8 @@ std::pair<int, std::string> SendTo(const int sockfd,
                                    const std::string& command)
 {
     const socklen_t addr_len{sizeof(dest_addr)};
-    int result =
-        sendto(sockfd, command.c_str(), command.size(), 0,
-               reinterpret_cast<sockaddr*>(&dest_addr), addr_len);
+    int result = sendto(sockfd, command.c_str(), command.size(), 0,
+                        reinterpret_cast<sockaddr*>(&dest_addr), addr_len);
 
     if (result == -1)
     {
--- src/ctello_cli.cpp
+++ src/ctello_cli.cpp
@@ -1,3 +1,21 @@
+//  CTello is a C++ library to interact with the DJI Ryze Tello Drone
+//  Copyright (C) 2020 Carlos Perez-Lopez
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>
+//
+//  You can contact the author via carlospzlz@gmail.com
+
 #include <iostream>
 
 #include "ctello.h"
@@ -87,8 +105,7 @@ int main()
             do
             {
                 response = tello.ReceiveResponse();
-            }
-            while (!response);
+            } while (!response);
             std::cout << *response << std::endl;
         }
         std::cout << PROMPT << std::flush;
